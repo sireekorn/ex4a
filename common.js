2cdea96307705c3252bade7b186a65531ed63d32
sireekorn <sirigon19990@gmail.com>
2024-02-08 11:27:29 +0700

    refactor#2 - add pceCal fn.

--- common.js
+++ common.js
@@ -9,7 +9,15 @@ function validate(pace){
 	}
 }
 
-
+function paceCal(pace,dist){
+	var min = dist * pace;
+		var hour = 0;
+		if(min > 60){
+			hour = Math.floor(min / 60);          
+			min = min % 60;
+}
+return {"min":min,"hour":hour};
+}
 function calculate(p,d){
 		var msg = "";
 		var pace = parseFloat(p);
@@ -18,12 +26,9 @@ function calculate(p,d){
 		var chk = validate(pace);
 		if(chk != null) return chk;
 	//---------------------------
-		var min = dist * pace;
-		var hour = 0;
-		if(min > 60){
-			hour = Math.floor(min / 60);          
-			min = min % 60;
-		}
+		
+	var pc = paceCal(pace,dist);
+		
 	//---------------------------
 		msg = "successfully calculated!"
 		return {"status":1,"message":msg,"hour":hour,"min":min};
